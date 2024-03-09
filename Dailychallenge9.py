f51674b4ab988256b41a01abcea255101adca469
MarlyT <79797180+Marilyn-Rose@users.noreply.github.com>
2024-03-09 20:47:15 +0100

    Day 9 challenge

--- Dailychallenge9.py
+++ Dailychallenge9.py
@@ -1,4 +1,17 @@
 # Write a function in Python that accepts a list of any length that 
 # contains a mix of non-negative integers and strings. 
-# The function should return a list with only the integers in the original 
-# list in the same order.
+# The function should return a list with only the integers in the original list in the same order.
+
+
+mixed_list = [10, 'apple', 20, 'banana', 30]
+
+def extract_int(input_list):
+    list_int = []
+    
+    for element in input_list:
+        if isinstance(element, int) and element >= 0:
+            list_int.append(element)
+    return list_int
+
+
+print (extract_int(mixed_list))
