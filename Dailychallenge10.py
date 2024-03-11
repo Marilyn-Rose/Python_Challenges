0609b1c5cf6e7ca523a4448ab88d8c030fa79b21
MarlyT <79797180+Marilyn-Rose@users.noreply.github.com>
2024-03-11 13:27:06 +0100

    Solved code for challenge 10

--- Dailychallenge10.py
+++ Dailychallenge10.py
@@ -3,3 +3,15 @@
 # string doubled.
 # If you send the function “now” as a parameter, it should return “nnooww,”
 # and if you send “123a!”, it should return “112233aa!!”.
+
+string_input = input(str("Enter string: "))
+
+def double_char(string_input):
+    double_string = ""
+    
+    for char in string_input:
+        double_string +=  char * 2
+    return double_string
+    
+print(double_char(string_input))
+        
